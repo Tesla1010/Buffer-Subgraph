efcacf74db151250591ce154fd29dcf954fa299d
Tesla <tesla@buffer.finance>
2023-04-24 21:24:58 +0530

    Fix

--- src/mapping.ts
+++ src/mapping.ts
@@ -29,42 +29,44 @@ export function handleCreate(event: Create): void {
 }
 
 export function handleClaim(event: Claim): void {
-  let claimDataPerUser = _loadOrCreateLBFRClaimDataPerUser(
-    event.params.account,
-    event.block.timestamp
-  );
-  claimDataPerUser.claimed = claimDataPerUser.claimed.plus(
-    event.params.claimedTokens
-  );
-  let LBFRStatTotal = _loadOrCreateLBFRStat(
-    "total",
-    event.block.timestamp,
-    event.params.account,
-    "total"
-  );
-  claimDataPerUser.claimable = LBFRStatTotal.lBFRAlloted.minus(
-    claimDataPerUser.claimed
-  );
-  let LBFRStatWeekly = _loadOrCreateLBFRStat(
-    "weekly",
-    event.block.timestamp,
-    event.params.account,
-    event.params.weekID.toString()
-  );
-  LBFRStatWeekly.claimed = LBFRStatWeekly.claimed.plus(
-    event.params.claimedTokens
-  );
-  LBFRStatWeekly.claimable = LBFRStatWeekly.claimable.minus(
-    event.params.claimedTokens
-  );
-  LBFRStatTotal.claimed = LBFRStatTotal.claimed.plus(
-    event.params.claimedTokens
-  );
-  LBFRStatTotal.claimable = LBFRStatTotal.claimable.minus(
-    event.params.claimedTokens
-  );
-  claimDataPerUser.lastClaimedTimestamp = event.block.timestamp;
-  claimDataPerUser.save();
-  LBFRStatWeekly.save();
-  LBFRStatTotal.save();
+  if (BigInt.fromI32(LBFR_START_TIMESTAMP) < event.block.timestamp) {
+    let claimDataPerUser = _loadOrCreateLBFRClaimDataPerUser(
+      event.params.account,
+      event.block.timestamp
+    );
+    claimDataPerUser.claimed = claimDataPerUser.claimed.plus(
+      event.params.claimedTokens
+    );
+    let LBFRStatTotal = _loadOrCreateLBFRStat(
+      "total",
+      event.block.timestamp,
+      event.params.account,
+      "total"
+    );
+    claimDataPerUser.claimable = LBFRStatTotal.lBFRAlloted.minus(
+      claimDataPerUser.claimed
+    );
+    let LBFRStatWeekly = _loadOrCreateLBFRStat(
+      "weekly",
+      event.block.timestamp,
+      event.params.account,
+      event.params.weekID.toString()
+    );
+    LBFRStatWeekly.claimed = LBFRStatWeekly.claimed.plus(
+      event.params.claimedTokens
+    );
+    LBFRStatWeekly.claimable = LBFRStatWeekly.claimable.minus(
+      event.params.claimedTokens
+    );
+    LBFRStatTotal.claimed = LBFRStatTotal.claimed.plus(
+      event.params.claimedTokens
+    );
+    LBFRStatTotal.claimable = LBFRStatTotal.claimable.minus(
+      event.params.claimedTokens
+    );
+    claimDataPerUser.lastClaimedTimestamp = event.block.timestamp;
+    claimDataPerUser.save();
+    LBFRStatWeekly.save();
+    LBFRStatTotal.save();
+  }
 }
